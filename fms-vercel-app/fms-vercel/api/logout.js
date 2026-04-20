aea1b71830ac124ca38cfb67d719eed2d9a99834
mosqueda.christopher <61306146+cmosqueda1@users.noreply.github.com>
2026-04-20 11:14:19 -0500

    Delete fms-vercel-app/fms-vercel directory

--- fms-vercel-app/fms-vercel/api/logout.js
+++
@@ -1,6 +0,0 @@
-const { json, clearSessionCookie } = require('./_lib/fms');
-
-module.exports = async (req, res) => {
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  return json(res, 200, { ok: true });
-};
